3f187dfd8929f5060ec7a3312fee6c9e0d9c57dc
Devdutt Shenoi <devdutt@outlook.in>
2020-04-18 23:41:04 +0530

    Refactor code to take multi line input

--- src/day_one.rs
+++ src/day_one.rs
@@ -1,5 +1,24 @@
 // Day 1: Tyranny of the Rocket Equation (https://adventofcode.com/2019/day/1)
+use std::io;
+use std::io::BufRead;
 
-pub fn sum_of_fuel_required(weight: u32) -> u32 {
+fn fuel_per_mod(weight: u32) -> u32 {
     weight/3-2
+}
+
+pub fn sum_of_fuel_required() -> u32 {
+    let stdin = io::stdin();
+    let mut sum: u32 = 0;
+    println!(r#"Input weights of modules as positive integers. 
+To finish, press `ENTER` on empty line of input."#);
+    for line in stdin.lock().lines() {
+        let input = line.unwrap();
+        let strip = input.trim();
+        match strip.parse::<u32>() {
+            Ok(i) => sum += fuel_per_mod(i),
+            Err(..) => break,
+        }
+    }
+
+    sum
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ mod day_one;
 use day_one::sum_of_fuel_required;
 
 fn main() {
-    let weight: u32 = 1969;
-    println!("weight of module: {} \nfuel required: {}", weight, sum_of_fuel_required(weight));
+    // Answer to q1
+    println!("fuel required: {}", sum_of_fuel_required());
 }
